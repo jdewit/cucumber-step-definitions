dffe415c5c22a33303df4f4a7280f0416e7183e9
Joris de Wit <joris.w.dewit@gmail.com>
2014-02-07 10:53:05 -0800

    use global vars to simplify usage

--- README.md
+++ README.md
@@ -1,30 +1,31 @@
 Cucumber Step Definitions
 =========================
 
-A collection of step definitions for use with <a href="https://github.com/cucumber/cucumber-js">cucumberjs</a>.
+A collection of step definitions for use with <a href="https://github.com/cucumber/cucumber-js">cucumberjs</a> and <href="https://github.com/angular/protractor">protractor</a>.
 These steps are highly specific to my apps. They will likely only be useful to you as a reference. 
 
 Usage
 -----
 
-Load a particular set of steps like so:
+Extend a particular set of steps like so:
 
 ```js
   // /project/features/step_definitions/form.js
 
-  var world = require('../support/world.js').World,
-      form = require('cucumber-step-definitions').form;
+  var form = require('cucumber-step-definitions').form;
 
   module.exports = function() {
-    this.World = world;
     form.call(this);
 
     // you can override steps or add custom steps of your own
 
     //this.Given(/^I am on "([^"]*)"$/, function(arg1, callback) {
-    //  this.browser.get(arg1 + 'some_custom_param').then(function() {
+    //  driver.get(arg1 + 'some_custom_param').then(function() {
     //    callback();
     //  });
     //});
   };
 ```
+
+##World.js
+Checkout world_example.js
--- world_example.js
+++ world_example.js
@@ -1,21 +1,37 @@
 var assert = require('chai').assert,
     path = require('path'),
-    browser = process.env.browser || 'phantomjs',
+    browserName = process.env.browser || 'chrome',
     protractor = require('protractor'),
-    webdriver = require('selenium-webdriver')
+    webdriver = require('selenium-webdriver'),
+    exec = require('child_process').exec,
+    util = require('util')
 ;
 
-var driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub').withCapabilities(webdriver.Capabilities[browser]()).build();
+var driver = new webdriver.Builder().usingServer('http://localhost:4444/wd/hub').withCapabilities(webdriver.Capabilities[browserName]()).build();
 
 driver.manage().timeouts().setScriptTimeout(100000);
 
-module.exports.World = function World(callback) {
-  this.browser = protractor.wrapDriver(driver);
-  this.By = protractor.By;
-  this.p = protractor;
-  this.assert = assert;
-  this.baseUrl = 'http://localhost:3000';
+var browser = protractor.wrapDriver(driver);
 
-  callback();
+module.exports = function() {
+  this.registerHandler('AfterFeatures', function(e, done) {
+    if (browserName === 'chrome') {
+      exec('pkill chromedriver');
+    } else if (browserName === 'phantomjs') {
+      exec('pkill phantomjs');
+    }
+
+    browser.quit();
+
+    setTimeout(function() {
+      done();
+    }, 1000);
+  });
 };
 
+global.driver = driver;
+global.browser = browser;
+global.p = protractor;
+global.by = protractor.By;
+global.assert = assert;
+global.baseUrl = 'http://www.google.com';
